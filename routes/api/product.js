74ddc6f220e9391c24602a7f200fd1800c1c64cb
brennedith <brennedithg@gmail.com>
2019-04-29 14:08:31 -0500

    Complete CRUD

--- routes/api/product.js
+++ routes/api/product.js
@@ -1,10 +1,27 @@
 const express = require('express');
-
-const router = express.Router();
+const mongoose = require('mongoose');
+const multer = require('multer');
 
 const Product = require('../../models/Product');
 
+const { ObjectId } = mongoose.Types;
+const upload = multer({ dest: './public/images/products' });
+const router = express.Router();
+
 // Create
+router.post('/', upload.single('image'), (req, res, next) => {
+  const { place, name, description, price } = req.body;
+
+  Product.create({
+    owner: ObjectId(place),
+    name,
+    description,
+    photoURL: `/images/products/${req.file.filename}`,
+    price
+  })
+    .then(product => res.send(product))
+    .catch(err => console.log(err));
+});
 
 // Read
 router.get('/', (req, res, next) => {
@@ -21,7 +38,31 @@ router.get('/:id', (req, res, next) => {
 });
 
 // Update
+router.patch('/:id', upload.single('image'), (req, res, next) => {
+  const { id } = req.params;
+  const { name, description, price, photoURL } = req.body;
+
+  Product.findByIdAndUpdate(
+    id,
+    {
+      name,
+      description,
+      photoURL: req.file ? `/images/products/${req.file.filename}` : photoURL,
+      price
+    },
+    { new: true }
+  )
+    .then(product => res.send(product))
+    .catch(err => console.log(err));
+});
 
 // Delete
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  Product.findByIdAndDelete(id)
+    .then(product => res.send(product))
+    .catch(err => console.log(err));
+});
 
 module.exports = router;
