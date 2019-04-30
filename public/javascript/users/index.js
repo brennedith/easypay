5a008eb73598960bbef7a220caad98961d91f4a7
brennedith <brennedithg@gmail.com>
2019-04-30 13:25:34 -0500

    Add create user modal

--- public/javascript/users/index.js
+++ public/javascript/users/index.js
@@ -1,17 +1,38 @@
 /* CRUD */
 
+// Create
+function createUser(data, cb) {
+  const $usersTable = document.querySelector('#users-list');
+
+  axios
+    .post(`${HOSTNAME}/api/user`, data, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    .then(({ data: user }) => {
+      $usersTable.innerHTML += renderUserRow(user);
+    });
+
+  cb();
+}
+
 // Read
 function readUsers() {
-  $usersTable = document.querySelector('#users');
+  const $usersTable = document.querySelector('#users-list');
+
   axios.get(`${HOSTNAME}/api/user`).then(({ data: users }) => {
     $usersTable.innerHTML = users.map(renderUserRow).join('');
   });
 }
 
+function readUser(id) {
+  axios.get(`${HOSTNAME}/api/user/${id}`).then(({ data: user }) => {});
+}
+
 // Update
 function editUser(el) {
   const { id } = el.dataset;
-  console.log(id);
 }
 
 // Delete
@@ -26,7 +47,50 @@ function deleteUser(el) {
   }
 }
 
+/* DOM Nodes */
+$addUser = document.getElementById('add-user');
+$addUserModal = document.getElementById('add-user-modal');
+$addUserClose = document.getElementById('add-user-close');
+$addUserCancel = document.getElementById('add-user-cancel');
+$addUserSubmit = document.getElementById('add-user-submit');
+
+// Event Listeners
+$addUser.onclick = () => openModal($addUserModal);
+$addUserClose.onclick = () => closeModal($addUserModal);
+$addUserCancel.onclick = () => closeModal($addUserModal);
+$addUserSubmit.onclick = () => submitModal('new', $addUserModal);
+
 /* Helpers */
+
+// Modal
+function openModal(el) {
+  el.classList.add('is-active');
+}
+
+function closeModal(el) {
+  el.classList.remove('is-active');
+}
+
+function submitModal(type, el) {
+  const formElements = [...el.querySelectorAll('input'), ...el.querySelectorAll('select')];
+  const formData = new FormData();
+
+  formElements.forEach(el => {
+    if (el.type === 'file') {
+      formData.append(el.name, el.files[0]);
+    } else {
+      formData.append(el.name, el.value);
+    }
+  });
+
+  if (type === 'new') {
+    createUser(formData, () => closeModal(el));
+  } else {
+    editUser(formData, () => closeModal(el));
+  }
+}
+
+// Renders
 function renderUserRow(user) {
   return `
     <tr id="${user._id}">
