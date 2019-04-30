ad06bdeaffbf0b1200453ce7bb149ea570c4a080
brennedith <brennedithg@gmail.com>
2019-04-30 12:13:27 -0500

    Add placeholder routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 // Landing page
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.send('Home page');
+});
+
+router.get('/dashboard', (req, res, next) => {
+  res.send('Dashboard');
+});
+
+// 404
+router.get('/not-found', (req, res, next) => {
+  res.send('That page does not exists');
+});
+
+// Error page
+router.get('/working-on-it', (req, res, next) => {
+  res.send('Something went wrong. :(');
 });
 
 module.exports = router;
