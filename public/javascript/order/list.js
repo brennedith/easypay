9934504d530edbe0b3de8401e0b7ab3035cea373
gustavvopenna <gustavo.pema@gmail.com>
2019-05-01 17:21:07 -0500

    Render order on orders list when creating new one

--- public/javascript/order/list.js
+++ public/javascript/order/list.js
@@ -1,6 +1,6 @@
 function createOrderHTML(order) {
   return `
-  <tr>
+  <tr id="${order._id}">
       <td>10:20</td>
       <td>${order._id}</td>
       <td>${order.total}</td>
@@ -19,6 +19,15 @@ function renderOrders() {
   axios.get(`${HOSTNAME}/api/order`).then(({ data: orders }) => {
     const { id, total, payments } = orders;
 
-    $orders.innerHTML = orders.map(order => createOrderHTML(order));
+    $orders.innerHTML = orders.map(order => createOrderHTML(order)).join('');
+  });
+}
+
+function createNewOrder() {
+  const $orders = document.getElementById('orders');
+
+  axios.post(`${HOSTNAME}/api/order`).then(({ data: orders }) => {
+    console.log(orders);
+    $orders.innerHTML += renderOrders();
   });
 }
