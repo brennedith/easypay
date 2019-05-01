1161d980d6826c5263898475bcaa175f191de2f9
gustavvopenna <gustavo.pema@gmail.com>
2019-05-01 09:39:52 -0500

    1. Create button to render products modal 2. Create modal to show products list

--- public/javascript/order/index.js
+++ public/javascript/order/index.js
@@ -6,78 +6,102 @@ function renderOrder(product) {
     <td>
       <div class="field has-addons">
         <div class="control">
-          <button data-id="${product._id}" onclick="updateInput(this, -1)" class="button is-danger"> - </button>
+          <button data-id="${
+            product._id
+          }" onclick="updateInput(this, -1)" class="button is-danger"> - </button>
         </div>
       
         <div class="control">
-          <input type="number" value="${product.qty}" data-id="${product._id}" oninput="updateOrder(this)" class="input">
+          <input type="number" value="${product.qty}" data-id="${
+    product._id
+  }" oninput="updateOrder(this)" class="input">
         </div>
       
         <div class="control">
-          <button data-id="${product._id}" onclick="updateInput(this, 1)" class="button is-success">+</button>
+          <button data-id="${
+            product._id
+          }" onclick="updateInput(this, 1)" class="button is-success">+</button>
         </div>
       </div>
     </td>
     <td>${product.subtotal.toFixed(2)}</td>
-  </tr>`
+  </tr>`;
 }
 
 function updateInput(el, n) {
-  const {dataset: { id }} = el
-  const $product = document.getElementById(id)
-  const $input  = $product.querySelector('input')
-  
-  $input.value = Number($input.value) + n
-  updateOrder($input)
+  const {
+    dataset: { id }
+  } = el;
+  const $product = document.getElementById(id);
+  const $input = $product.querySelector("input");
+
+  $input.value = Number($input.value) + n;
+  updateOrder($input);
 }
 
 function updateOrder(el) {
- const {
-   dataset: { id },
-   value: qty
- } = el;
+  const {
+    dataset: { id },
+    value: qty
+  } = el;
 
- const $product = document.getElementById(id)
+  const $product = document.getElementById(id);
 
- axios
-  .patch(`${HOSTNAME}/api/order/5cc76c8544c5d850589801f4`, {
-   product: id,
-   quantity: Number(qty)
- })
- .then(({ data: order }) => {
-  if(qty <= 0 ) {
-    $product.parentElement.removeChild($product)
-  } else {
-    const { products, productsQty} = order
-    let productIndex;
-  
-    products.forEach((product, index) => {
-      if (product._id === id) {
-        productIndex = index;
+  axios
+    .patch(`${HOSTNAME}/api/order/5cc76c8544c5d850589801f4`, {
+      product: id,
+      quantity: Number(qty)
+    })
+    .then(({ data: order }) => {
+      if (qty <= 0) {
+        $product.parentElement.removeChild($product);
+      } else {
+        const { products, productsQty } = order;
+        let productIndex;
+
+        products.forEach((product, index) => {
+          if (product._id === id) {
+            productIndex = index;
+          }
+        });
+
+        const product = products[productIndex];
+        product.qty = productsQty[productIndex];
+        product.subtotal = product.price * product.qty;
+
+        document.getElementById(id).outerHTML = renderOrder(product);
       }
     });
-  
-    const product = products[productIndex]
-    product.qty = productsQty[productIndex]
-    product.subtotal = product.price * product.qty
-  
-    document.getElementById(id).outerHTML = renderOrder(product)
-  }
-
- });
-} 
+}
 
 function renderOrders() {
-  $products = document.getElementById('products')
+  $products = document.getElementById("products");
 
-  axios.get(`${HOSTNAME}/api/order/5cc76c8544c5d850589801f4`)
-  .then(({ data: order }) => {
-    const { products, productsQty } = order
+  axios
+    .get(`${HOSTNAME}/api/order/5cc76c8544c5d850589801f4`)
+    .then(({ data: order }) => {
+      const { products, productsQty } = order;
 
-    $products.innerHTML = products.map((product, index) => {
-      product.qty = productsQty[index]
-      product.subtotal = product.price * product.qty
-      return renderOrder(product)
-    }).join("");
-  })
+      $products.innerHTML = products
+        .map((product, index) => {
+          product.qty = productsQty[index];
+          product.subtotal = product.price * product.qty;
+          return renderOrder(product);
+        })
+        .join("");
+    });
+}
+
+// ---------- MODAL -------------
+
+function openModal() {
+  const $modal = document.getElementById("modal-ter");
+
+  $modal.classList.add("is-active");
+}
+
+function closeModal() {
+  const $modal = document.getElementById("modal-ter");
+
+  $modal.classList.remove("is-active");
 }
