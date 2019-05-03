a9e0282ef94b7e3c8dac01f0950d4258f9f3146e
brennedith <brennedithg@gmail.com>
2019-05-03 12:03:12 -0500

    Order now shows a breakdown of payments

--- public/javascript/order/list.js
+++ public/javascript/order/list.js
@@ -1,10 +1,17 @@
 function createOrderHTML(order) {
+  const payments = order.payments
+    .map(payment => {
+      const className = payment.complete ? 'is-success' : 'is-warning';
+      return `<strong class="tag ${className}">${payment.amount.toFixed(2)}</strong>`;
+    })
+    .join(' ');
+
   return `
   <tr id="${order._id}">
       <td>${order.createdAt.slice(0, 16).replace('T', ' / ')}</td>
       <td>${order._id.slice(-5)}</td>
       <td>${order.total.toFixed(2)}</td>
-      <td>${order.complete}</td>
+      <td>${payments}</td>
       <td>
       <a href="${HOSTNAME}/orders/${order._id}">
         <button class="button is-link is-small">View</button>
@@ -17,7 +24,6 @@ function renderOrders() {
   const $orders = document.getElementById('orders');
 
   axios.get(`${HOSTNAME}/api/order`).then(({ data: orders }) => {
-    const { id, total, payments } = orders;
     $orders.innerHTML = orders.map(order => createOrderHTML(order)).join('');
   });
 }
